6ccb06343574a75200c58eda094ae6695e3d3cca
Groctel <groctel@protonmail.com>
2021-04-26 17:17:25 +0200

    [SG] Añadido ejercicio 6

---
+++ 3º 2º cuatrimestre/Sistemas gráficos/Práctica 1 - Ejercicios/Ejercicio 6 - Carga de modelos obj/My3DModel.js
@@ -0,0 +1,34 @@
+import * as THREE from '../libs/three.module.js'
+
+import { OBJLoader } from '../libs/OBJLoader.js'
+import { MTLLoader } from '../libs/MTLLoader.js'
+
+class My3DModel extends THREE.Object3D
+{
+	constructor (obj_path, mtl_path)
+	{
+		super();
+
+		const obj_loader = new OBJLoader();
+		const mtl_loader = new MTLLoader();
+
+		mtl_loader.load(mtl_path, (mtl) =>
+		{
+			mtl.preload();
+			obj_loader.setMaterials(mtl);
+			obj_loader.load(obj_path, (root) =>
+			{
+				root.scale.set(5, 5, 5);
+				root.translateY(3);
+				this.add(root);
+			});
+		});
+	}
+
+	update ()
+	{
+		this.rotation.y += 0.01;
+	}
+}
+
+export { My3DModel };
